08b737ac2c1610c72c07851566a45a73e230657a
Phillip Demro <phillip.demro@hotmail.com>
2017-12-19 09:54:02 -0500

    updates per guidance from @waldekm

--- spfx_1-4-x/spfx-1-4-x/src/extensions/helloWorld/HelloWorldApplicationCustomizer.ts
+++ spfx_1-4-x/spfx-1-4-x/src/extensions/helloWorld/HelloWorldApplicationCustomizer.ts
@@ -23,8 +23,8 @@ export interface IHelloWorldApplicationCustomizerProperties {
 export default class HelloWorldApplicationCustomizer
   extends BaseApplicationCustomizer<IHelloWorldApplicationCustomizerProperties> {
 
-    private _topPlaceholder: PlaceholderContent | undefined;
-    private _bottomPlaceholder: PlaceholderContent | undefined;
+  private _topPlaceholderLegacy: PlaceholderContent | undefined;
+  private _bottomPlaceholder: PlaceholderContent | undefined;
 
   @override
   public onInit(): Promise<void> {
@@ -32,40 +32,55 @@ export default class HelloWorldApplicationCustomizer
     console.log('Available placeholders: ',
       this.context.placeholderProvider.placeholderNames.map(name => PlaceholderName[name]).join(', '));
 
-    this.context.placeholderProvider.changedEvent.add(this, this.renderTop);
     this.context.application.navigatedEvent.add(this, this.renderTop)
-    this.context.application._layoutChangedEvent.add(this, this.renderTop);
-
-    this.renderTop();
+    //this.context.placeholderProvider.changedEvent.add(this, this.renderTop);
+    //this.context.application._layoutChangedEvent.add(this, this.renderTop);
+    //this.renderTop();
 
     return Promise.resolve();
   }
 
-
-
+  //Thank you Waldek! https://github.com/SharePoint/sp-dev-docs/issues/1042#issuecomment-352686900
   private renderTop() {
-
     // Handling the top placeholder
-    if (!this._topPlaceholder) {
-      this._topPlaceholder =
+    const topPlaceholder =
+      this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Top,
+        { onDispose: this._onDispose });
+
+    // The extension should not assume that the expected placeholder is available.
+    if (!topPlaceholder) {
+      console.error('The expected placeholder (Top) was not found.');
+      return;
+    }
+
+    if (topPlaceholder.domElement) {
+      topPlaceholder.domElement.innerHTML = `Hello World spfx1.4 1.0.0.2`
+    }
+  }
+
+  // This example comes from the online example which may be flawed https://github.com/SharePoint/sp-dev-docs/blob/master/docs/spfx/extensions/get-started/using-page-placeholder-with-extensions.md
+  private renderTopLegacy() {
+    // Handling the top placeholder
+    if (!this._topPlaceholderLegacy) {
+      this._topPlaceholderLegacy =
         this.context.placeholderProvider.tryCreateContent(
           PlaceholderName.Top,
           { onDispose: this._onDispose });
 
       // The extension should not assume that the expected placeholder is available.
-      if (!this._topPlaceholder) {
+      if (!this._topPlaceholderLegacy) {
         console.error('The expected placeholder (Top) was not found.');
         return;
       }
 
-      if (this._topPlaceholder.domElement) {
-        this._topPlaceholder.domElement.innerHTML = `Hello World spfx1.4 1.0.0.0`
+      if (this._topPlaceholderLegacy.domElement) {
+        this._topPlaceholderLegacy.domElement.innerHTML = `Hello World spfx1.4 1.0.0.0`
       }
-      
+
     }
-
-
   }
+
 
   private _onDispose(): void {
     console.log('[HelloWorldApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
